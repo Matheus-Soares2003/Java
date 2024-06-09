4ab7e99a1c27444e65dc66eb05c144003762ca19
Matheus Cabral <matheusstcabral@gmail.com>
2024-06-09 17:09:50 -0300

    Exercicio de Desconto

---
+++ OperadoresAritmeticos/Desconto.java
@@ -0,0 +1,21 @@
+package OperadoresAritmeticos;
+
+import java.util.Scanner;
+
+public class Desconto {
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Preço do produto: R$");
+        float preco = scan.nextFloat();
+
+        //desconto de 5%
+        float desconto = preco - preco * 0.05f;
+
+        System.out.println(String.format("O produto que custava R$%.2f com desconto de 5%% vai custar R$%.2f", preco, desconto));
+
+        scan.close();
+
+    }
+}
